1af4027ef360c89f8f1621760a133c0fcb7ceace
Bula2 <bulaev123da@gmail.com>
2023-12-24 00:08:26 +0300

    updates

--- src/pages/CreateForecast/CreateForecast.tsx
+++ src/pages/CreateForecast/CreateForecast.tsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
-import { Col, Row, Typography, message, Upload } from 'antd';
-import { MyUploader } from './components/Uploader/MyUploader';
+import { Button, Form, Typography, message } from 'antd';
 import { Link } from 'react-router-dom';
+import { MyUploader } from './components/Uploader/MyUploader';
 
+import axios from 'axios';
 import styles from './CreateForecast.module.scss';
 
 const { Title, Text } = Typography;
 
 export const CreateForecast = () => {
-  const [categories, setCategories] = useState<string[]>([]);
-  const [data, setData] = useState<number[]>([]);
-  console.log(data);
-  console.log(categories);
+  const onFinish = async (values: any) => {
+    try {
+      await axios.post(
+        'http://127.0.0.1:8000/api/file/add/',
+        { file: values.file[0].originFileObj },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      message.success(`Прогноз создан!`);
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
   return (
-    <div>
-      <Title level={3}>{'Создание прогноза'}</Title>
-      <Text className={styles.linkInstructions}>
-        {'Перед создание прогноза ознакомтесь с '}
-        <Link to="/instructions">{'Инструкцией'}</Link>
-      </Text>
-      <MyUploader
-        data={data}
-        categories={categories}
-        setData={setData}
-        setCategories={setCategories}
-      />
-    </div>
+    <Form name="forecast" onFinish={onFinish}>
+      <div>
+        <Title level={3}>{'Создание прогноза'}</Title>
+        <Text className={styles.linkInstructions}>
+          {'Перед создание прогноза ознакомьтесь с '}
+          <Link to="/instructions">{'Инструкцией'}</Link>
+        </Text>
+        <MyUploader />
+        <Form.Item style={{ marginTop: 50 }}>
+          <Button type="primary" htmlType="submit">
+            {'Создать прогноз'}
+          </Button>
+        </Form.Item>
+      </div>
+    </Form>
   );
 };
